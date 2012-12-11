07f915a0fab9c838d2585ab1c5d5afdc87ba2162
jhugon <>
2012-12-11 02:31:05 +0000

    fixed cafFolder name bug

--- UserArea/crab_scripts/submit.py
+++ UserArea/crab_scripts/submit.py
@@ -50,7 +50,7 @@ if isMC:
             else:
               print("Error: No MC HLT Trigger or CMSSW version for Energy: '%s' TeV" % s.energy)
               sys.exit(1)
-            cafFolder = "higgs/%s/%s/Ntuples/%s/" % (cmsswString,s.ntuple,"MC")
+            cafFolder = "higgs/%s/%s/Ntuples/%s/" % (cmsswString,Samples.tag,"MC")
             command += './createCrab.py -c %s -d %s -n %s -g %s -e %s -f %s --hlt %s ' \
                        % (cmsswString,
                           s.dataset,
@@ -71,7 +71,7 @@ else:
             else:
               print("Error: No Data CMSSW version for Energy: '%s' TeV" % s.energy)
               sys.exit(1)
-            cafFolder = "higgs/%s/%s/Ntuples/%s/" % (cmsswString,s.ntuple,"MC")
+            cafFolder = "higgs/%s/%s/Ntuples/%s/" % (cmsswString,Samples.tag,"MC")
             command += './createCrab.py -c %s -d %s -n %s -g %s -e %s -f %s -j %s ' \
                        % (cmsswString,
                           s.dataset,
