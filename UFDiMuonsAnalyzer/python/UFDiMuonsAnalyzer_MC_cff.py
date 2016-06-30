16d15a2cf2a5ef86cfb7ea30a365229b71b7f962
Andrew Carnes <carnesandnoble@gmail.com>
2016-06-30 11:54:11 +0200

    Forgot to add the python scripts to the repo.

---
+++ UFDiMuonsAnalyzer/python/UFDiMuonsAnalyzer_MC_cff.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+## Assumes you are looking at miniAOD
+
+DiMuons = cms.EDAnalyzer('UFDiMuonsAnalyzer',
+                         muonColl = cms.InputTag("slimmedMuons"),
+                         
+                         isVerbose = cms.untracked.bool(False),
+                         isMonteCarlo = cms.bool(True),
+                         nMuons = cms.int32(2),
+                         
+                         # muon kinematic cuts
+                         isGlobal    =cms.int32(0),
+                         isStandAlone=cms.int32(0),
+                         isTracker   =cms.int32(1),
+                         ptMin       = cms.double(10),
+                         etaMax      = cms.double(2.4),
+    
+                         primaryVertexTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                         beamSpotTag = cms.InputTag("offlineBeamSpot"),
+                         prunedGenParticleTag = cms.InputTag("prunedGenParticles"),
+                         packedGenParticleTag = cms.InputTag("packedGenParticles"),
+                         
+                         #triggerName = cms.string("@"),#wild card: all triggers
+                         # HLT trigger info
+                         checkTrigger = cms.bool(False),
+
+                         processName = cms.string("HLT"),
+                         # Not sure why only 3 names, haven't tried more
+                         triggerNames = cms.vstring("HLT_IsoMu20","HLT_IsoTkMu20","HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ"), # up to 3 trigger names
+
+                         triggerResults = cms.InputTag("TriggerResults","","HLT"),
+                         triggerObjs = cms.InputTag("selectedPatTrigger"),
+                         
+                         metTag         = cms.InputTag("slimmedMETs"),
+                         pfJetsTag      = cms.InputTag("slimmedJets"),
+                         genJetsTag     = cms.InputTag("slimmedGenJets")
+                         )
